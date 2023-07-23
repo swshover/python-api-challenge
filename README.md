f4ab10a0c4f238196e5bc01b674f7f6db5f4774f
swshover <135381688+swshover@users.noreply.github.com>
2023-07-23 17:45:40 -0500

    done

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # python-api-challenge
-Homework
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "f6c52212e6729c288b042790fe2a4b3a"
+
+# Geoapify API Key
+geoapify_key = "ae2ad391884942d1b2430c242265e2c7"
